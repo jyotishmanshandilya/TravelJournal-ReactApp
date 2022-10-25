a5e96445f84d606933e04c1646f2de77040463e5
Jyotishman Shandilya <92297562+jyotishmanshandilya@users.noreply.github.com>
2022-10-25 11:16:56 +0530

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,9 +4,7 @@ export default function Intro(){
             <p className="intro--para">
                 This is my personal travel journal where i'll be sharing all the destinations 
                 I've visited in the past and mark them with a few stats for reference.
-                I'll add some dummy pictures for now and later append them with some actual 
-                pictures from the trips.
-                Hope it turns out as good as I can imagine it to be right now - '30th september, 2022'.
+                
             </p>
         </div>
     )
